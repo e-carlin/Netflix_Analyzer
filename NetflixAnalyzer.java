1d3f039462b4e4c6a07d17536d403245c711fce2
MBogert <mgilbert311@gmail.com>
2016-11-29 16:06:50 -0800

    Set up

--- NetflixAnalyzer.java
+++ NetflixAnalyzer.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Netflix Analyzer that allows users to explore
  * Mark Gilbert and Evan Carlin
@@ -6,6 +10,15 @@
 public class NetflixAnalyzer {
 
     public static void main(String[] args){
+        //Get the input from the user
+        System.out.println("Please specify which file you would like to analyze");
+        Scanner sc = new Scanner(System.in);
+        try{
 
+            File nf = new File(sc.next());
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
